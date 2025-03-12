7301809bf021306a7d18a1e13019d9069ee212a6
lat9 <lat9@vinosdefrutastropicales.com>
2025-03-12 10:06:45 -0400

    #3: Update manifest to point to plugin on zen-cart.com

--- zc_plugins/LocalSalesTax/v3.0.0/manifest.php
+++ zc_plugins/LocalSalesTax/v3.0.0/manifest.php
@@ -5,7 +5,7 @@ return [
     'pluginDescription' => 'This plugin adds the support for local sales tax zones. In the United States a local sales tax is a additional tax levied by the county or city in addition to the tax levied
 by a state.',
     'pluginAuthor' => 'lat9',
-    'pluginId' => 0, // ID from Zen Cart forum
+    'pluginId' => 422, // ID from Zen Cart forum
     'zcVersions' => [],
     'changelog' => '', // online URL (eg github release tag page, or changelog file there) or local filename only, ie: changelog.txt (in same dir as this manifest file)
     'github_repo' => '', // url
