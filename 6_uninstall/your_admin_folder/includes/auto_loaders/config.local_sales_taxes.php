adbff92d580d96907ade78e1b23a9bd01bc38c94
lat9 <lat9@vinosdefrutastropicales.com>
2025-03-12 09:56:33 -0400

    #3: Restructure as encapsulated plugin

--- 6_uninstall/your_admin_folder/includes/auto_loaders/config.local_sales_taxes.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!defined('IS_ADMIN_FLAG')) {
-    die('Illegal Access');
-}
-
-$autoLoadConfig[999][] = [
-  'autoType' => 'init_script',
-  'loadFile' => 'init_local_sales_taxes_uninstall.php'
-];
